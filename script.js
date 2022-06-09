6afcf6f04380179d9573e6e7649d7efc3722f368
PhuongPhan95 <phanp95@uw.edu>
2022-06-08 22:49:36 -0700

    Update comments

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
     // check if an element is in viewport
     // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
     function isElementInViewport(el) {
+      //set value to get the view of client
       var rect = el.getBoundingClientRect();
+      //return value for the function
       return (
+        //set condition
         rect.top >= 0 &&
         rect.left >= 0 &&
         rect.bottom <=
@@ -14,9 +17,11 @@
       );
     }
 
+    // callbackFunc is a function call the timeline box out when elements are in the view area.
     function callbackFunc() {
       // define variables
       var items = document.querySelectorAll(".timeline li");
+      //for loop run through the list of timeline boxes
       for (var i = 0; i < items.length; i++) {
         if (isElementInViewport(items[i])) {
           items[i].classList.add("in-view");
